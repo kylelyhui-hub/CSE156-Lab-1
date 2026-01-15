f941132ba551e17d62e950f8a70e1de872fe2101
kylelyhui-hub <kylelyhui@gmail.com>
2026-01-15 10:07:06 +0000

    Delete lecture 1 exercises directory

--- lecture 1 exercises/script1.py
+++
@@ -1,6 +0,0 @@
-#script for converting km to miles and printing the miles out
-marathon_km = 42.195;
-convertMilesToKm = 1.61;
-marathon_miles = marathon_km / convertMilesToKm;
-print(marathon_miles,"miles");
-
--- lecture 1 exercises/script2.py
+++
@@ -1,5 +0,0 @@
-#script for converting 100 USD to GBP
-dollars = 100;
-exchangeRate = 0.75;
-pounds = dollars * exchangeRate;
-print(pounds,'GBP');
