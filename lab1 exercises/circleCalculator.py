76d50ffffbc615d8ec77d553ca8496629d60702b
kylelyhui-hub <kylelyhui@gmail.com>
2026-01-15 10:08:21 +0000

    Add files via upload

---
+++ lab1 exercises/circleCalculator.py
@@ -0,0 +1,12 @@
+import math;
+#Interactive script to find the area of a circle
+pi = math.pi;
+r = input('Please enter the radius');
+radius = float(r);
+area = pi*radius*radius;
+print('The area of the circle is',area);
+
+circumference = 2*pi*radius;
+print('The circumference of the circle is',circumference);
+
+
---
+++ lecture 1 exercises/script1.py
@@ -0,0 +1,6 @@
+#script for converting km to miles and printing the miles out
+marathon_km = 42.195;
+convertMilesToKm = 1.61;
+marathon_miles = marathon_km / convertMilesToKm;
+print(marathon_miles,"miles");
+
